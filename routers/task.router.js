0cf76afa491dfb8cd943789d5fc445287efd5f0f
iammayur99 <147247475+mayur756@users.noreply.github.com>
2024-09-18 09:26:38 +0530

    task.router.js

--- routers/task.router.js
+++ routers/task.router.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { getUserTasks, filter } = require('../controllers/task.controller');
 
-router.get('/tasks/:userId', getUserTasks);
-router.get('/filter', filter);
+router.get('/updtasks/', getUserTasks);
+
 
 module.exports = router;
